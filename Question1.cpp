3fe57a7b72dd563fd7ae0e4b58b10b6e8bd5dec5
RO169 <rowen.rungiah@gmail.com>
2015-04-18 13:51:29 +0200

    1.4/1.5

--- Question1.cpp
+++ Question1.cpp
@@ -26,8 +26,8 @@ int main(int argc, char *argv[]) {
 	cin >> name;
 	cout << "Hello, " << name << endl;
 
-	// 1.4 merge branch "hello" into "master" keeping ALL of the code added so far
+	// 1.4 merge branch "hello" into "master" keeping ALL of the code added so far.
 
-	// 1.5 make a merge commit 
+	// 1.5 make a merge commit.
 
 }
